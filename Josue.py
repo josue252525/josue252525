8c0574411e3e77655ed1e8b0ffedf6580035db43
josue252525 <142546145+josue252525@users.noreply.github.com>
2023-08-17 19:58:50 -0300

    Update Josue.py

--- Josue.py
+++ Josue.py
@@ -1,10 +1,4 @@
-- 👋 Hi, I’m @josue252525
-- 👀 I’m interested in ...
-- 🌱 I’m currently learning ...
-- 💞️ I’m looking to collaborate on ...
-- 📫 How to reach me ...
-
-<!---
-josue252525/josue252525 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
-You can click the Preview link to take a look at your changes.
---->
+josue.python3
+ls
+python3.py
+Oi josue bem vindo a nosso novo comando
